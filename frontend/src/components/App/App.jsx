d827fbc4e8ea577b6f26e3e863780e7fb9b61477
Marlon GARCIA <clasesucatmarlon@gmail.com>
2023-08-13 16:54:30 -0500

    feat: :zap: test1
    test1

--- frontend/src/components/App/App.jsx
+++ frontend/src/components/App/App.jsx
@@ -3,7 +3,7 @@ import '../../assets/css/App.css'
 const App = () => {
 	return (
 		<div>
-			<h1 className='bg-primary'> Hello Marlon GARCIA this is a test run of
+			<h1 className='bg-primary'> Hello Marlon GARCIA this is a test run of 22233
 			</h1>
 		</div>
 	);
