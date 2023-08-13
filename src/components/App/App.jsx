d306af4e5232d9f89f6800ecb8ddfd7758a1b554
NasserAbuchaibe <nasser_abuchaibe@hotmail.com>
2023-08-13 18:11:08 -0500

    test  app

---
+++ src/components/App/App.jsx
@@ -0,0 +1,15 @@
+import '../../assets/css/App.css';
+
+const App = () => {
+	let x = 5;
+	return (
+		<div>
+			<h1 className='bg-primary'>
+				{' '}
+				Hello Marlon GARCIA this is a test run of 22233
+			</h1>
+		</div>
+	);
+};
+
+export default App
